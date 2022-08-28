28b1d3f1aa4b5a116e3cef77e486b942620e50f6
Sam <sam.s@itstacks.net>
2022-08-28 03:13:54 +0100

    sample form table added + logout

---
+++ src/Controller/FormController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/dashboard')]
+class FormController extends AbstractController
+{
+    #[Route('/form', name: 'app_form')]
+    public function index(): Response
+    {
+        return $this->render('form/index.html.twig', [
+            'controller_name' => 'FormController',
+        ]);
+    }
+}
